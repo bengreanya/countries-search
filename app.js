b3566906eaf161fc76dcbf4a4559f2a390e49f55
Ben Greanya <bengreanya@gmail.com>
2022-11-09 12:06:39 -0800

    countries displayed on page load

--- app.js
+++ app.js
@@ -25,10 +25,13 @@ window.addEventListener('load', async () => {
 
 async function findCountries(continent) {
     // Slice A: call the asynchronous fetch function to get the countries
+    const response = await getCountries();
     // Slice C: add continent argument to getCountries function call
     // console log the response object to see all of the nested information returned
     // Slice A: set the countries state to the response.data
+    countries = response.data;
     // Slice A: call displayCountries function;
+    displayCountries();
 }
 
 searchForm.addEventListener('submit', (e) => {
